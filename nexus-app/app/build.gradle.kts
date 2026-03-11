ef19bc9816782803d296c234bc3ee36f5d341ae1
Dreiz0x <cisne170990@gmail.com>
2026-03-10 22:10:37 -0600

    Update build.gradle.kts

--- nexus-app/app/build.gradle.kts
+++ nexus-app/app/build.gradle.kts
@@ -101,7 +101,7 @@ dependencies {
     }
     implementation("org.apache.poi:poi-ooxml:5.2.3") {
         exclude(group = "com.google.guava")
-    
+    }
     implementation("cz.adaptech.tesseract4android:tesseract4android:4.7.0")
 
     // 6. RED
